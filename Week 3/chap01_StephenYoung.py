f47a6b4df8520874fe34be292534a6bfe4270c09
StephenTYoung <95554685+StephenTYoung@users.noreply.github.com>
2022-06-26 18:53:48 -0400

    Week 3

---
+++ Week 3/chap01_StephenYoung.py
@@ -0,0 +1,65 @@
+'''
+Created: Stephen Young
+Assignment: Week 3 Exercise 1-2
+Date: 6/26/22
+'''
+
+from __future__ import print_function, division
+
+import numpy as np
+import sys
+
+import nsfg
+import thinkstats2
+
+
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+
+def CleanFemResp(df):
+
+    pass
+
+
+def ValidatePregnum(resp):
+
+    # read the pregnancy frame
+    preg = nsfg.ReadFemPreg()
+
+    # make the map from caseid to list of pregnancy indices
+    preg_map = nsfg.MakePregMap(preg)
+
+    # iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.items():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+
+        # check that pregnum from the respondent file equals
+        # the number of records in the pregnancy file
+        if len(indices) != pregnum:
+            print(caseid, len(indices), pregnum)
+            return False
+
+    return True
+
+
+def main(script):
+
+    resp = ReadFemResp()
+
+    assert(len(resp) == 7643)
+    assert(resp.pregnum.value_counts()[1] == 1267)
+    assert(ValidatePregnum(resp))
+
+    print('%s: All tests passed.' % script)
+
+
+if __name__ == '__main__':
+    main(*sys.argv)
---
+++ Week 3/chap02_StephenYoung.py
@@ -0,0 +1,77 @@
+"""
+Stephen Young
+Assignment: Week 3, 2-2
+6/26/22
+"""
+
+from __future__ import print_function
+
+import sys
+from operator import itemgetter
+
+import first
+import thinkstats2
+
+
+def Mode(hist):
+    p, x = max([(p, x) for x, p in hist.Items()])
+    return x
+
+
+def AllModes(hist):
+
+    return sorted(hist.Items(), key=itemgetter(1), reverse=True)
+
+
+def WeightDifference(live, firsts, others):
+
+    mean0 = live.totalwgt_lb.mean()
+    mean1 = firsts.totalwgt_lb.mean()
+    mean2 = others.totalwgt_lb.mean()
+
+    var1 = firsts.totalwgt_lb.var()
+    var2 = others.totalwgt_lb.var()
+
+    print('Mean')
+    print('First babies', mean1)
+    print('Others', mean2)
+
+    print('Variance')
+    print('First babies', var1)
+    print('Others', var2)
+
+    print('Difference in lbs', mean1 - mean2)
+    print('Difference in oz', (mean1 - mean2) * 16)
+
+    print('Difference relative to mean (%age points)',
+          (mean1 - mean2) / mean0 * 100)
+
+    d = thinkstats2.CohenEffectSize(firsts.totalwgt_lb, others.totalwgt_lb)
+    print('Cohen d', d)
+
+
+def main(script):
+
+    live, firsts, others = first.MakeFrames()
+    hist = thinkstats2.Hist(live.prglngth)
+
+    # explore the weight difference between first babies and others
+    WeightDifference(live, firsts, others)
+
+    # test Mode
+    mode = Mode(hist)
+    print('Mode of preg length', mode)
+    assert(mode == 39)
+
+    # test AllModes
+    modes = AllModes(hist)
+    assert(modes[0][1] == 4693)
+
+    for value, freq in modes[:5]:
+        print(value, freq)
+
+    print('%s: All tests passed.' % script)
+
+
+if __name__ == '__main__':
+    main(*sys.argv)
